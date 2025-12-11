7426016f0a55740e6f29eaf8a34ff475d6dc7eb6
gokulrajc8393 <gokulrajc63@gmail.com>
2025-12-11 21:25:34 +0530

    upload page

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')   
 
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
